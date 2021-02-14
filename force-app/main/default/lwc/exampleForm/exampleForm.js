1b0e3702e559fa8d6cb4240aecec3ba08f8326bb
Raul Juliao <raulj@uninorte.edu.co>
2021-02-14 14:38:02 -0500

    Lightning Web Components

--- force-app/main/default/lwc/exampleForm/exampleForm.js
+++ force-app/main/default/lwc/exampleForm/exampleForm.js
@@ -1,3 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import LNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import FNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import PHONE_FIELD from '@salesforce/schema/Contact.Phone';
 
-export default class ExampleForm extends LightningElement {}
+export default class ExampleForm extends LightningElement {
+
+    // objectApiName is 'Contact' when is placed in the conact record page
+    @api objectApiName;
+    fields = [FNAME_FIELD, LNAME_FIELD, EMAIL_FIELD, PHONE_FIELD];
+
+    handleSuccess(event){
+        const evt = new ShowToastEvent({
+            title: "Contact created",
+            message: "Record ID: " + event.detail.id,
+            variant: "success"
+        })
+        this.dispatchEvent(evt);
+    }
+}
